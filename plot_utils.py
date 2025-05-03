5e3ecea8d2582e405a6c86ff8e18b699ce2fd6b7
Rachanon-Andrew-Petchoo <90673520+Rachanon-Andrew-Petchoo@users.noreply.github.com>
2025-05-03 18:41:55 -0500

    fix: Plotting functions + Provide all plots
    * Modified: Plotting functions
    
    * Added: All plots

--- plot_utils.py
+++ plot_utils.py
@@ -13,29 +13,41 @@ def load_results(result_filepaths):
     if isinstance(result_filepaths, str):
         result_filepaths = [result_filepaths]
     dfs = [pd.read_csv(filepath) for filepath in result_filepaths]
-    return pd.concat(dfs, ignore_index=True)
+    df = pd.concat(dfs, ignore_index=True)
+
+    # Normalize for graphs that plots "dimension", "dataset size"
+    df['build_time_s_per_point'] = df['build_time_s'] / df['train_size']
+    df['memory_mb_per_point'] = df['memory_mb'] / df['train_size']
+    df['build_time_s_per_dim'] = df['build_time_s'] / df['dimension']
+    df['memory_mb_per_dim'] = df['memory_mb'] / df['dimension']
+
+    return df
 
 
-def plot_metric_vs_param(result_filepaths, x_param, metric, split_by='dataset', hue_param='dimension', save_dir=None):
+def plot_metric_vs_param(result_filepaths, x_param, metric, hue_param=None, save_dir=None):
     results_df = load_results(result_filepaths)
 
     plt.figure(figsize=(10, 7))
-    palette = sns.color_palette("tab10")
-    sns.lineplot(data=results_df, x=x_param, y=metric, hue=hue_param,
-                 style=split_by, markers=True, dashes=False, palette=palette)
+    palette = sns.color_palette("tab10", 5)
+    if hue_param:
+        sns.lineplot(data=results_df, x=x_param, y=metric, hue=hue_param,
+                 markers=True, dashes=False, palette=palette)
+    else: # Combine datasets
+        sns.lineplot(data=results_df, x=x_param, y=metric, markers=True)
     plt.xlabel(x_param)
     plt.ylabel(metric)
     plt.title(f"{metric} vs {x_param}")
     plt.grid(True)
 
     # Add text labels
-    texts = []
-    for line in results_df.groupby([hue_param, split_by]):
-        group = line[1].sort_values(by=x_param)
-        last = group.iloc[-1]
-        label = f"{line[0][0]} - {line[0][1]}"
-        texts.append(plt.text(last[x_param], last[metric], label, fontsize=9))
-    adjust_text(texts, arrowprops=dict(arrowstyle="->", color='gray'))
+    if hue_param:
+        texts = []
+        for line in results_df.groupby([hue_param]):
+            group = line[1].sort_values(by=x_param)
+            last = group.iloc[-1]
+            label = f"{line[0][0]}"
+            texts.append(plt.text(last[x_param], last[metric], label, fontsize=9))
+        adjust_text(texts, arrowprops=dict(arrowstyle="->", color='gray'))
 
     if save_dir:
         os.makedirs(save_dir, exist_ok=True)
@@ -43,39 +55,7 @@ def plot_metric_vs_param(result_filepaths, x_param, metric, split_by='dataset', 
         plt.savefig(save_path)
         print(f"[Plot] Saved to {save_path}")
 
-
-def plot_scaling(result_filepaths, metric, save_dir=None):
-    results_df = load_results(result_filepaths)
-
-    plt.figure(figsize=(10, 7))
-    palette = sns.color_palette("tab10")
-    ax = sns.lineplot(data=results_df, x='train_size', y=metric,
-                      hue='dimension', markers=True, dashes=False, palette=palette)
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.xlabel('Dataset Size (log scale)')
-    plt.ylabel(metric)
-    plt.title(f"{metric} vs Dataset Size")
-    plt.grid(True, which="both", ls="--")
-
-    # Add text labels
-    texts = []
-    for line in results_df.groupby('dimension'):
-        group = line[1].sort_values(by='train_size')
-        last = group.iloc[-1]
-        label = str(line[0])
-        texts.append(plt.text(last['train_size'],
-                     last[metric], label, fontsize=9))
-    adjust_text(texts, arrowprops=dict(arrowstyle="->", color='gray'))
-
-    if save_dir:
-        os.makedirs(save_dir, exist_ok=True)
-        save_path = os.path.join(save_dir, f"{metric}_vs_dataset_size.png")
-        plt.savefig(save_path)
-        print(f"[Plot] Saved to {save_path}")
-
-
-def plot_3d_surface(result_filepaths, x_param, y_param, metric, save_dir=None):
+def plot_2_metric_vs_param(result_filepaths, x_param, y_param, metric, save_dir=None):
     results_df = load_results(result_filepaths)
 
     aggregated = (
@@ -107,7 +87,7 @@ def plot_3d_surface(result_filepaths, x_param, y_param, metric, save_dir=None):
         print(f"[Plot] Saved to {save_path}")
 
 
-def plot_3rd_scatter_color(result_filepaths, x_param, y_param, z_param, metric, save_dir=None):
+def plot_3_metric_vs_param(result_filepaths, x_param, y_param, z_param, metric, save_dir=None):
     """
     Plots a 3D scatter plot with color mapping for a 4th dimension (metric).
     Averages metric values for duplicate (x, y, z) points.
@@ -148,67 +128,79 @@ if __name__ == "__main__":
     result_filepaths = ['results/sift-128_results.csv', 'results/mnist_results.csv',
                         'results/last.fm_results.csv', 'results/nytimes_results.csv', 'results/coco-i2i_results.csv']
 
-    plot_scaling(result_filepaths, metric='build_time_s',
-                 save_dir="plots/scaling")
-    plot_scaling(result_filepaths, metric='memory_mb',
-                 save_dir="plots/scaling")
+    plot_metric_vs_param(result_filepaths, x_param='train_size',
+                         metric='memory_mb_per_dim', save_dir="plots/train_size")
+    plot_metric_vs_param(result_filepaths, x_param='train_size',
+                         metric='build_time_s_per_dim', save_dir="plots/train_size")
 
     plot_metric_vs_param(result_filepaths, x_param='dimension',
-                         metric='memory_mb', split_by='dataset', save_dir="plots/dimension")
+                         metric='memory_mb_per_point', save_dir="plots/dimension")
     plot_metric_vs_param(result_filepaths, x_param='dimension',
-                         metric='avg_latency_ms', split_by='dataset', save_dir="plots/dimension")
+                         metric='build_time_s_per_point', save_dir="plots/dimension")
+    plot_metric_vs_param(result_filepaths, x_param='dimension',
+                         metric='avg_latency_ms', save_dir="plots/dimension")
 
     plot_metric_vs_param(result_filepaths, x_param='efConstruction',
-                         metric='build_time_s', split_by='dataset', save_dir="plots/ef_construct")
+                         metric='build_time_s', hue_param='dataset', save_dir="plots/ef_construct")
     plot_metric_vs_param(result_filepaths, x_param='efConstruction',
-                         metric='memory_mb', split_by='dataset', save_dir="plots/ef_construct")
+                         metric='memory_mb', hue_param='dataset', save_dir="plots/ef_construct")
     plot_metric_vs_param(result_filepaths, x_param='efConstruction',
-                         metric='recall', split_by='dataset', save_dir="plots/ef_construct")
+                         metric='recall', hue_param='dataset', save_dir="plots/ef_construct")
 
     plot_metric_vs_param(result_filepaths, x_param='efSearch',
-                         metric='recall', split_by='dataset', save_dir="plots/ef_search")
+                         metric='recall', hue_param='dataset', save_dir="plots/ef_search")
     plot_metric_vs_param(result_filepaths, x_param='efSearch',
-                         metric='avg_latency_ms', split_by='dataset', save_dir="plots/ef_search")
+                         metric='memory_mb', hue_param='dataset', save_dir="plots/ef_search")
+    plot_metric_vs_param(result_filepaths, x_param='efSearch',
+                         metric='avg_latency_ms', hue_param='dataset', save_dir="plots/ef_search")
 
     plot_metric_vs_param(result_filepaths, x_param='M',
-                         metric='build_time_s', split_by='dataset', save_dir="plots/M")
+                         metric='build_time_s', hue_param='dataset', save_dir="plots/M")
     plot_metric_vs_param(result_filepaths, x_param='M',
-                         metric='memory_mb', split_by='dataset', save_dir="plots/M")
+                         metric='memory_mb', hue_param='dataset', save_dir="plots/M")
     plot_metric_vs_param(result_filepaths, x_param='M',
-                         metric='recall', split_by='dataset', save_dir="plots/M")
+                         metric='recall', hue_param='dataset', save_dir="plots/M")
+    plot_metric_vs_param(result_filepaths, x_param='M',
+                         metric='avg_latency_ms', hue_param='dataset', save_dir="plots/M")
 
-    plot_3d_surface(result_filepaths, x_param='dimension', y_param='M',
-                    metric='build_time_s', save_dir="plots/combined")
-    plot_3d_surface(result_filepaths, x_param='dimension', y_param='efConstruction',
-                    metric='build_time_s', save_dir="plots/combined")
-    plot_3d_surface(result_filepaths, x_param='efConstruction',
-                    y_param='M', metric='build_time_s', save_dir="plots/combined")
+    plot_2_metric_vs_param(result_filepaths, x_param='dimension', y_param='M',
+                           metric='build_time_s_per_point', save_dir="plots/3D")
+    plot_2_metric_vs_param(result_filepaths, x_param='dimension', y_param='efConstruction',
+                           metric='build_time_s_per_point', save_dir="plots/3D")
+    plot_2_metric_vs_param(result_filepaths, x_param='efConstruction', y_param='M', 
+                           metric='build_time_s', save_dir="plots/3D")
 
-    plot_3d_surface(result_filepaths, x_param='dimension',
-                    y_param='M', metric='memory_mb', save_dir="plots/combined")
-    plot_3d_surface(result_filepaths, x_param='dimension',
-                    y_param='efConstruction', metric='memory_mb', save_dir="plots/combined")
-    plot_3d_surface(result_filepaths, x_param='efConstruction',
-                    y_param='M', metric='memory_mb', save_dir="plots/combined")
+    plot_2_metric_vs_param(result_filepaths, x_param='dimension', y_param='M', 
+                           metric='memory_mb_per_point', save_dir="plots/3D")
+    plot_2_metric_vs_param(result_filepaths, x_param='dimension', y_param='efConstruction', 
+                           metric='memory_mb_per_point', save_dir="plots/3D")
+    plot_2_metric_vs_param(result_filepaths, x_param='efConstruction', y_param='M', 
+                           metric='memory_mb', save_dir="plots/3D")
 
-    plot_3d_surface(result_filepaths, x_param='efSearch',
-                    y_param='M', metric='recall', save_dir="plots/combined")
-    plot_3d_surface(result_filepaths, x_param='efConstruction',
-                    y_param='M', metric='recall', save_dir="plots/combined")
-    plot_3d_surface(result_filepaths, x_param='efConstruction',
-                    y_param='efSearch', metric='recall', save_dir="plots/combined")
+    plot_2_metric_vs_param(result_filepaths, x_param='efSearch', y_param='M', 
+                           metric='recall', save_dir="plots/3D")
+    plot_2_metric_vs_param(result_filepaths, x_param='efConstruction', y_param='M', 
+                           metric='recall', save_dir="plots/3D")
+    plot_2_metric_vs_param(result_filepaths, x_param='efConstruction', y_param='efSearch', 
+                           metric='recall', save_dir="plots/3D")
 
-    plot_3d_surface(result_filepaths, x_param='efSearch', y_param='M',
-                    metric='avg_latency_ms', save_dir="plots/combined")
-    plot_3d_surface(result_filepaths, x_param='efSearch', y_param='dimension',
-                    metric='avg_latency_ms', save_dir="plots/combined")
-    plot_3d_surface(result_filepaths, x_param='dimension', y_param='M',
-                    metric='avg_latency_ms', save_dir="plots/combined")
+    plot_2_metric_vs_param(result_filepaths, x_param='efSearch', y_param='M',
+                           metric='avg_latency_ms', save_dir="plots/3D")
+    plot_2_metric_vs_param(result_filepaths, x_param='efSearch', y_param='dimension',
+                           metric='avg_latency_ms', save_dir="plots/3D")
+    plot_2_metric_vs_param(result_filepaths, x_param='dimension', y_param='M',
+                           metric='avg_latency_ms', save_dir="plots/3D")
     
-    plot_3d_surface(result_filepaths, x_param='efConstruction', y_param='M',
-                    metric='throughput_qps', save_dir="plots/combined")
+    plot_2_metric_vs_param(result_filepaths, x_param='efConstruction', y_param='M',
+                           metric='throughput_qps', save_dir="plots/3D")
 
-    plot_3rd_scatter_color(result_filepaths, x_param='efSearch', y_param='efConstruction',
-                           z_param='M', metric='avg_latency_ms', save_dir="plots/combined")
-    plot_3rd_scatter_color(result_filepaths, x_param='efSearch', y_param='efConstruction',
-                           z_param='M', metric='recall', save_dir="plots/combined")
+    plot_3_metric_vs_param(result_filepaths, x_param='efSearch', y_param='efConstruction',z_param='M', 
+                           metric='build_time_s', save_dir="plots/4D")
+    plot_3_metric_vs_param(result_filepaths, x_param='efSearch', y_param='efConstruction',z_param='M', 
+                           metric='memory_mb', save_dir="plots/4D")
+    plot_3_metric_vs_param(result_filepaths, x_param='efSearch', y_param='efConstruction',z_param='M', 
+                           metric='avg_latency_ms', save_dir="plots/4D")
+    plot_3_metric_vs_param(result_filepaths, x_param='efSearch', y_param='efConstruction',z_param='M', 
+                           metric='throughput_qps', save_dir="plots/4D")
+    plot_3_metric_vs_param(result_filepaths, x_param='efSearch', y_param='efConstruction',z_param='M', 
+                           metric='recall', save_dir="plots/4D")
